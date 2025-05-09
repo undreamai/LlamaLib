a249250c7166311edd171b8bdd798d7de032faf7
Antonios Makropoulos <benuix@gmail.com>
2025-05-09 16:21:34 +0300

    code and naming improvements

--- dynamic_loader.h
+++ dynamic_loader.h
@@ -36,26 +36,26 @@ using LibHandle = void*;
 #define LOADER_API
 #endif
 
+//=================================== FUNCTION LISTS ===================================//
+
 // Forward declarations
 struct LLM;
-struct LLMBackend;
+struct LLMLib;
 
-// Function lists
-
-#define BACKEND_FUNCTIONS_WITH_NOLLM_NOARGS(X) \
+#define LLMLIB_FUNCTIONS_NOLLM_NOARGS(X) \
     X(StopLogging, void) \
     X(StringWrapper_Construct, StringWrapper*) \
 
-#define BACKEND_FUNCTIONS_WITH_NOLLM_ONEARG(X) \
+#define LLMLIB_FUNCTIONS_NOLLM_ONEARG(X) \
     X(Logging, void, StringWrapper*) \
     X(StringWrapper_Delete, void, StringWrapper*) \
     X(StringWrapper_GetStringSize, int, StringWrapper*) \
     X(LLM_Construct, LLM*, const char*)
 
-#define BACKEND_FUNCTIONS_WITH_NOLLM_FOURARGS(X) \
+#define LLMLIB_FUNCTIONS_NOLLM_FOURARGS(X) \
     X(StringWrapper_GetString, void, StringWrapper*, char*, int, bool)
 
-#define BACKEND_FUNCTIONS_WITH_LLM_NOARGS(X) \
+#define LLMLIB_FUNCTIONS_LLM_NOARGS(X) \
     X(LLM_Delete, void, LLM*) \
     X(LLM_Start, void, LLM*) \
     X(LLM_Started, bool, LLM*) \
@@ -64,12 +64,12 @@ struct LLMBackend;
     X(LLM_StopServer, void, LLM*) \
     X(LLM_Embedding_Size, int, LLM*)
 
-#define BACKEND_FUNCTIONS_WITH_LLM_ONEARG(X) \
+#define LLMLIB_FUNCTIONS_LLM_ONEARG(X) \
     X(LLM_Lora_List, void, LLM*, StringWrapper*) \
     X(LLM_Cancel, void, LLM*, int) \
     X(LLM_Status, int, LLM*, StringWrapper*)
 
-#define BACKEND_FUNCTIONS_WITH_LLM_TWOARGS(X) \
+#define LLMLIB_FUNCTIONS_LLM_TWOARGS(X) \
     X(LLM_SetSSL, void, LLM*, const char*, const char*) \
     X(LLM_Tokenize, void, LLM*, const char*, StringWrapper*) \
     X(LLM_Detokenize, void, LLM*, const char*, StringWrapper*) \
@@ -78,26 +78,29 @@ struct LLMBackend;
     X(LLM_Completion, void, LLM*, const char*, StringWrapper*) \
     X(LLM_Slot, void, LLM*, const char*, StringWrapper*)
 
-#define BACKEND_FUNCTIONS_ALL(X) \
-    BACKEND_FUNCTIONS_WITH_NOLLM_NOARGS(X) \
-    BACKEND_FUNCTIONS_WITH_NOLLM_ONEARG(X) \
-    BACKEND_FUNCTIONS_WITH_NOLLM_FOURARGS(X) \
-    BACKEND_FUNCTIONS_WITH_LLM_NOARGS(X) \
-    BACKEND_FUNCTIONS_WITH_LLM_ONEARG(X) \
-    BACKEND_FUNCTIONS_WITH_LLM_TWOARGS(X)
+#define LLMLIB_FUNCTIONS_ALL(X) \
+    LLMLIB_FUNCTIONS_NOLLM_NOARGS(X) \
+    LLMLIB_FUNCTIONS_NOLLM_ONEARG(X) \
+    LLMLIB_FUNCTIONS_NOLLM_FOURARGS(X) \
+    LLMLIB_FUNCTIONS_LLM_NOARGS(X) \
+    LLMLIB_FUNCTIONS_LLM_ONEARG(X) \
+    LLMLIB_FUNCTIONS_LLM_TWOARGS(X)
 
 // Typedefs
 #define DECLARE_TYPEDEF(name, ret, ...) typedef ret (*name##_Fn)(__VA_ARGS__);
-    BACKEND_FUNCTIONS_ALL(DECLARE_TYPEDEF)
+    LLMLIB_FUNCTIONS_ALL(DECLARE_TYPEDEF)
 #undef DECLARE_TYPEDEF
 
-struct LLMBackend {
-#define DECLARE_FIELD(name, ret, ...) name##_Fn name##_fn;
-        BACKEND_FUNCTIONS_ALL(DECLARE_FIELD)
-#undef DECLARE_FIELD
+//=================================== LLMLib ===================================//
 
-    LibHandle handle = nullptr;
-    LLM* llm = nullptr;
+struct LLMLib {
+        ~LLMLib();
+        LibHandle handle = nullptr;
+        LLM* llm = nullptr;
+
+#define DECLARE_FIELD(name, ret, ...) name##_Fn name##_fn;
+        LLMLIB_FUNCTIONS_ALL(DECLARE_FIELD)
+#undef DECLARE_FIELD
 
 #define DECLARE_METHOD_NOLLM_NOARGS(name, ret) \
     inline ret name() { return name##_fn(); }
@@ -105,7 +108,7 @@ struct LLMBackend {
 #define DECLARE_METHOD_NOLLM_ONEARG(name, ret, arg1_type) \
     inline ret name(arg1_type arg1) { return name##_fn(arg1); }
 
-#define DECLARE_METHOD_WITH_NOLLM_FOURARGS(name, ret, arg1_type, arg2_type, arg3_type, arg4_type) \
+#define DECLARE_METHOD_NOLLM_FOURARGS(name, ret, arg1_type, arg2_type, arg3_type, arg4_type) \
     inline ret name(arg1_type arg1, arg2_type arg2, arg3_type arg3, arg4_type arg4) { return name##_fn(arg1, arg2, arg3, arg4); }
 
 #define DECLARE_METHOD_LLM_NOARGS(name, ret, ...) \
@@ -117,33 +120,78 @@ struct LLMBackend {
 #define DECLARE_METHOD_LLM_TWOARGS(name, ret, _, arg1_type, arg2_type) \
     inline ret name(arg1_type arg1, arg2_type arg2) { return name##_fn(llm, arg1, arg2); }
 
-BACKEND_FUNCTIONS_WITH_NOLLM_NOARGS(DECLARE_METHOD_NOLLM_NOARGS)
-BACKEND_FUNCTIONS_WITH_NOLLM_ONEARG(DECLARE_METHOD_NOLLM_ONEARG)
-BACKEND_FUNCTIONS_WITH_NOLLM_FOURARGS(DECLARE_METHOD_WITH_NOLLM_FOURARGS)
-BACKEND_FUNCTIONS_WITH_LLM_NOARGS(DECLARE_METHOD_LLM_NOARGS)
-BACKEND_FUNCTIONS_WITH_LLM_ONEARG(DECLARE_METHOD_LLM_ONEARG)
-BACKEND_FUNCTIONS_WITH_LLM_TWOARGS(DECLARE_METHOD_LLM_TWOARGS)
+LLMLIB_FUNCTIONS_NOLLM_NOARGS(DECLARE_METHOD_NOLLM_NOARGS)
+LLMLIB_FUNCTIONS_NOLLM_ONEARG(DECLARE_METHOD_NOLLM_ONEARG)
+LLMLIB_FUNCTIONS_NOLLM_FOURARGS(DECLARE_METHOD_NOLLM_FOURARGS)
+LLMLIB_FUNCTIONS_LLM_NOARGS(DECLARE_METHOD_LLM_NOARGS)
+LLMLIB_FUNCTIONS_LLM_ONEARG(DECLARE_METHOD_LLM_ONEARG)
+LLMLIB_FUNCTIONS_LLM_TWOARGS(DECLARE_METHOD_LLM_TWOARGS)
 
 #undef DECLARE_METHOD_NOLLM_NOARGS
 #undef DECLARE_METHOD_NOLLM_ONEARG
-#undef DECLARE_METHOD_WITH_NOLLM_FOURARGS
+#undef DECLARE_METHOD_NOLLM_FOURARGS
 #undef DECLARE_METHOD_LLM_NOARGS
 #undef DECLARE_METHOD_LLM_ONEARG
 #undef DECLARE_METHOD_LLM_TWOARGS
 };
 
-//============================= WRAPPER FUNCTIONS =============================//
+//=================================== HELPERS ===================================//
 
-// GPU Enum
 enum GPU {
     NO_GPU = 0,
     TINYBLAS = 1,
     CUDA = 2
 };
 
-// Loader functions
-const std::vector<std::string> get_possible_architectures_array(GPU gpu);
-LOADER_API const char* get_possible_architectures(GPU gpu);
-LOADER_API bool load_llm_backend(const std::string& path, LLMBackend& backend);
-LOADER_API int load_backends_fallback(GPU gpu, std::string command, LLMBackend& backend);
-LOADER_API void unload_llm_backend(LLMBackend& backend);
+std::string join_paths(const std::string& a, const std::string& b);
+const std::vector<std::string> available_architectures(GPU gpu);
+
+//=================================== EXTERNAL API ===================================//
+
+LOADER_API const char* Available_Architectures(GPU gpu);
+LOADER_API LLMLib* Load_LLM_Library_From_Path(const std::string& path);
+LOADER_API LLMLib* Load_LLM_Library(GPU gpu, std::string command, const std::string& baseDir="");
+
+#define EXPORT_WRAPPER_NOLLM_NOARGS(name, ret) \
+extern "C" inline LOADER_API ret LlamaLib_##name(LLMLib* llmlib) { \
+    return llmlib->name(); \
+}
+
+#define EXPORT_WRAPPER_NOLLM_ONEARG(name, ret, arg1_type) \
+extern "C" inline LOADER_API ret LlamaLib_##name(LLMLib* llmlib, arg1_type arg1) { \
+    return llmlib->name(arg1); \
+}
+
+#define EXPORT_WRAPPER_NOLLM_FOURARGS(name, ret, arg1_type, arg2_type, arg3_type, arg4_type) \
+extern "C" inline LOADER_API ret LlamaLib_##name(LLMLib* llmlib, arg1_type arg1, arg2_type arg2, arg3_type arg3, arg4_type arg4) { \
+    return llmlib->name(arg1, arg2, arg3, arg4); \
+}
+
+#define EXPORT_WRAPPER_LLM_NOARGS(name, ret, _) \
+extern "C" inline LOADER_API ret LlamaLib_##name(LLMLib* llmlib) { \
+    return llmlib->name(); \
+}
+
+#define EXPORT_WRAPPER_LLM_ONEARG(name, ret, _, arg1_type) \
+extern "C" inline LOADER_API ret LlamaLib_##name(LLMLib* llmlib, arg1_type arg1) { \
+    return llmlib->name(arg1); \
+}
+
+#define EXPORT_WRAPPER_LLM_TWOARGS(name, ret, _, arg1_type, arg2_type) \
+extern "C" inline LOADER_API ret LlamaLib_##name(LLMLib* llmlib, arg1_type arg1, arg2_type arg2) { \
+    return llmlib->name(arg1, arg2); \
+}
+
+LLMLIB_FUNCTIONS_NOLLM_NOARGS(EXPORT_WRAPPER_NOLLM_NOARGS)
+LLMLIB_FUNCTIONS_NOLLM_ONEARG(EXPORT_WRAPPER_NOLLM_ONEARG)
+LLMLIB_FUNCTIONS_NOLLM_FOURARGS(EXPORT_WRAPPER_NOLLM_FOURARGS)
+LLMLIB_FUNCTIONS_LLM_NOARGS(EXPORT_WRAPPER_LLM_NOARGS)
+LLMLIB_FUNCTIONS_LLM_ONEARG(EXPORT_WRAPPER_LLM_ONEARG)
+LLMLIB_FUNCTIONS_LLM_TWOARGS(EXPORT_WRAPPER_LLM_TWOARGS)
+
+#undef EXPORT_WRAPPER_NOLLM_NOARGS
+#undef EXPORT_WRAPPER_NOLLM_ONEARG
+#undef EXPORT_WRAPPER_NOLLM_FOURARGS
+#undef EXPORT_WRAPPER_LLM_NOARGS
+#undef EXPORT_WRAPPER_LLM_ONEARG
+#undef EXPORT_WRAPPER_LLM_TWOARGS
