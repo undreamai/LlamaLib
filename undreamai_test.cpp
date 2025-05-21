339f3fb05d53f5c1950611b301d1f39d161a3c22
Antonios Makropoulos <benuix@gmail.com>
2025-05-21 14:36:13 +0300

    separate main into unit tests

--- undreamai_test.cpp
+++ undreamai_test.cpp
@@ -1,5 +1,16 @@
 #include "LLM_service.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#include <limits.h>
+#endif
+
+#include <iostream>
+
+int ID_SLOT = 0;
+
 #define ASSERT(cond) \
     do { \
         if (!(cond)) { \
@@ -54,108 +65,165 @@ std::string concatenate_streaming_result(std::string input)
     return output;
 }
 
-int main(int argc, char ** argv) {
-    LLMService* llm;
-    StringWrapper* stringWrapper = StringWrapper_Construct();
-    std::string prompt = "you are an artificial intelligence assistant\n\n### user: Hello, how are you?\n### assistant";
-    std::string command = "";
-    for (int i = 1; i < argc; ++i) {
-        command += argv[i];
-        if (i < argc - 1) command += " ";
-    }
-    json data;
-    json reply_data;
+void test_tokenization(LLMService* llm, StringWrapper* wrapper, const std::string& prompt) {
+    std::cout << "******* LLM_Tokenize *******" << std::endl;
+    json data, reply_data;
     std::string reply;
-    int id_slot = 0;
 
-    std::cout<<"******* LLM_Construct *******"<<std::endl;
-    llm = LLM_Construct(command.c_str());
-
-    std::thread t([&]() {LLM_Start(llm);return 1;});
-    std::cout<<"******* LLM_Started *******"<<std::endl;
-    while(!LLM_Started(llm)){}
-    
-    std::cout<<"******* LLM_Tokenize *******"<<std::endl;
     data["content"] = prompt;
-    LLM_Tokenize(llm, data.dump().c_str(), stringWrapper);
-    reply = GetStringWrapperContent(stringWrapper);
+    LLM_Tokenize(llm, data.dump().c_str(), wrapper);
+    reply = GetStringWrapperContent(wrapper);
     reply_data = json::parse(reply);
     ASSERT(reply_data.count("tokens") > 0);
     ASSERT(reply_data["tokens"].size() > 0);
 
-    std::cout<<"******* LLM_Detokenize *******"<<std::endl;
-    LLM_Detokenize(llm, reply.c_str(), stringWrapper);
-    reply = GetStringWrapperContent(stringWrapper);
+    std::cout << "******* LLM_Detokenize *******" << std::endl;
+    LLM_Detokenize(llm, reply.c_str(), wrapper);
+    reply = GetStringWrapperContent(wrapper);
     reply_data = json::parse(reply);
     ASSERT(trim(reply_data["content"]) == data["content"]);
+}
 
-    std::cout<<"******* LLM_Completion *******"<<std::endl;
-    data.clear();
-    data["id_slot"] = id_slot;
+void test_completion(LLMService* llm, StringWrapper* wrapper, const std::string& prompt, bool stream) {
+    std::cout << "******* LLM_Completion ( ";
+    if (!stream) std::cout << "no ";
+    std::cout << "streaming ) *******" << std::endl;
+    json data;
+    std::string reply;
+
+    data["id_slot"] = ID_SLOT;
     data["prompt"] = prompt;
     data["cache_prompt"] = true;
     data["stream"] = false;
-    data["n_predict"] = 50;
+    data["n_predict"] = 10;
     data["n_keep"] = 30;
-    LLM_Completion(llm, data.dump().c_str(), stringWrapper);
-    reply = GetStringWrapperContent(stringWrapper);
-    reply_data = json::parse(reply);
-    ASSERT(reply_data.count("content") > 0);
+    data["stream"] = stream;
 
-    data["prompt"] = prompt + std::string(reply_data["content"]);
-
-    LLM_Tokenize(llm, reply.c_str(), stringWrapper);
-    reply = GetStringWrapperContent(stringWrapper);
-    reply_data = json::parse(reply);
-    std::cout << std::abs((float)data["n_predict"] - reply_data["tokens"].size()) << std::endl;
-    ASSERT(std::abs((float)data["n_predict"] - reply_data["tokens"].size()) < 4);
-
-    std::cout<<"******* LLM_Completion 2 *******"<<std::endl;
-    data["stream"] = true;
-    LLM_Completion(llm, data.dump().c_str(), stringWrapper);
-    reply = GetStringWrapperContent(stringWrapper);
-    reply_data = concatenate_streaming_result(reply);
+    LLM_Completion(llm, data.dump().c_str(), wrapper);
+    reply = GetStringWrapperContent(wrapper);
+    std::string reply_data;
+    if (stream)
+    {
+        reply_data = concatenate_streaming_result(reply);
+    }
+    else {
+        reply_data = json::parse(reply)["content"];
+    }
     ASSERT(reply_data != "");
+}
 
-    std::cout<<"******* LLM_Embeddings *******"<<std::endl;
+void test_embedding(LLMService* llm, StringWrapper* wrapper, const std::string& prompt) {
+    std::cout << "******* LLM_Embeddings *******" << std::endl;
+    json data, reply_data;
+    std::string reply;
+
     data["content"] = prompt;
-    LLM_Embeddings(llm, data.dump().c_str(), stringWrapper);
-    reply = GetStringWrapperContent(stringWrapper);
+    LLM_Embeddings(llm, data.dump().c_str(), wrapper);
+    reply = GetStringWrapperContent(wrapper);
     reply_data = json::parse(reply);
+
     ASSERT(reply_data["embedding"].size() == LLM_Embedding_Size(llm));
+}
 
-    std::cout<<"******* LLM_Lora_List *******"<<std::endl;
-    LLM_Lora_List(llm, stringWrapper);
-    reply = GetStringWrapperContent(stringWrapper);
-    reply_data = json::parse(reply);
+void test_lora(LLMService* llm, StringWrapper* wrapper) {
+    std::cout << "******* LLM_Lora_List *******" << std::endl;
+    LLM_Lora_List(llm, wrapper);
+    std::string reply = GetStringWrapperContent(wrapper);
+    json reply_data = json::parse(reply);
     ASSERT(reply_data.size() == 0);
+}
 
-    std::cout<<"******* LLM_Cancel *******"<<std::endl;
-    LLM_Cancel(llm, id_slot);
+void test_cancel(LLMService* llm) {
+    std::cout << "******* LLM_Cancel *******" << std::endl;
+    LLM_Cancel(llm, ID_SLOT);
+}
 
-    /*
+void test_slot_save_restore(LLMService* llm, StringWrapper* wrapper) {
+    std::cout << "******* LLM_Slot Save *******" << std::endl;
     std::string filename = "test_undreamai.save";
-    data.clear();
-    data["id_slot"] = id_slot;
+    json data;
+    json reply_data;
+    std::string reply;
+
+    data["id_slot"] = ID_SLOT;
     data["action"] = "save";
-    data["filename"] = filename;
-    LLM_Slot(llm, data.dump().c_str());
+
+#ifdef _WIN32
+    char buffer[MAX_PATH];
+    GetCurrentDirectoryA(MAX_PATH, buffer);
+    data["filepath"] = std::string(buffer) + "\\" + filename;
+#else
+    char buffer[PATH_MAX];
+    getcwd(buffer, sizeof(buffer));
+    data["filepath"] = std::string(buffer) + "/" + filename;
+#endif
+   
+    LLM_Slot(llm, data.dump().c_str(), wrapper);
+    reply = GetStringWrapperContent(wrapper);
+    reply_data = json::parse(reply);
+    ASSERT(reply_data["filename"] == filename);
+    int n_saved = reply_data["n_saved"];
+    ASSERT(n_saved > 0);
+
     std::ifstream f(filename);
     ASSERT(f.good());
     f.close();
 
+    std::cout << "******* LLM_Slot Restore *******" << std::endl;
     data["action"] = "restore";
-    LLM_Slot(llm, data.dump().c_str());
-    std::remove(filename.c_str());
-    */
+    LLM_Slot(llm, data.dump().c_str(), wrapper);
+    reply = GetStringWrapperContent(wrapper);
+    std::cout << reply << std::endl;
+    reply_data = json::parse(reply);
+    ASSERT(reply_data["filename"] == filename);
+    ASSERT(reply_data["n_restored"] == n_saved);
 
-    std::cout<<"******* LLM_StopServer *******"<<std::endl;
+    std::remove(filename.c_str());
+}
+
+std::pair<LLMService*, std::thread> start_llm(const std::string& command)
+{
+    LLMService* llm = LLM_Construct(command.c_str());
+    std::thread t([&]() { LLM_Start(llm); return 1; });
+    while (!LLM_Started(llm)) {}
+    return { llm, std::move(t) };
+}
+
+void stop_llm(LLMService* llm, std::thread& t)
+{
+    std::cout << "******* LLM_StopServer *******" << std::endl;
     LLM_StopServer(llm);
-    std::cout<<"******* LLM_Stop *******"<<std::endl;
+    std::cout << "******* LLM_Stop *******" << std::endl;
     LLM_Stop(llm);
     t.join();
-    std::cout<<"******* LLM_Delete *******"<<std::endl;
+    std::cout << "******* LLM_Delete *******" << std::endl;
     LLM_Delete(llm);
+}
+
+int main(int argc, char** argv) {
+    std::string prompt = "you are an artificial intelligence assistant\n\n### user: Hello, how are you?\n### assistant";
+    std::string command;
+    for (int i = 1; i < argc; ++i) {
+        command += argv[i];
+        if (i < argc - 1) command += " ";
+    }
+
+    StringWrapper* wrapper = StringWrapper_Construct();
+
+    auto res = start_llm(command);
+    LLMService* llm = res.first;
+    std::thread t = std::move(res.second);
+
+    test_tokenization(llm, wrapper, prompt);
+    test_completion(llm, wrapper, prompt, false);
+    test_completion(llm, wrapper, prompt, true);
+    test_embedding(llm, wrapper, prompt);
+    test_lora(llm, wrapper);
+    test_cancel(llm);
+    test_slot_save_restore(llm, wrapper);
+
+    stop_llm(llm, t);
+    delete wrapper;
 
     return 0;
 }
