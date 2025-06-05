f82f4ab48b39b377f2f3b178eff60fe564cf74b6
Antonis Makropoulos <benuix@gmail.com>
2025-06-05 12:04:59 +0300

    add LLM_runtime files

---
+++ LLM_runtime.cpp
@@ -0,0 +1,190 @@
+#include "LLM_runtime.h"
+
+//============================= LIBRARY LOADING =============================//
+
+const std::vector<std::string> available_architectures(bool gpu) {
+    std::vector<std::string> architectures;
+
+    const auto add_library = [&](std::string os, std::string arch, std::string prefix, std::string suffix)
+    {
+        std::string dash_arch = arch;
+        if (arch != "") dash_arch = "_" + dash_arch;
+        std::string path = prefix + "llamalib_" + os + dash_arch + "." + suffix;
+        architectures.push_back(path);
+    };
+
+    std::string prefix = "";
+    std::string suffix = "";
+    std::string os = "";
+
+#if defined(_WIN32) || defined(__linux__)
+#if defined(_WIN32)
+    prefix = "";
+    suffix = "dll";
+    os = "windows";
+#elif defined(__linux__)
+    prefix = "lib";
+    suffix = "so";
+    os = "linux";
+#endif
+    if (gpu) {
+        add_library(os, "cublas", prefix, suffix);
+        add_library(os, "tinyblas", prefix, suffix);
+        add_library(os, "hip", prefix, suffix);
+        add_library(os, "vulkan", prefix, suffix);
+    }
+    if (has_avx512()) add_library(os, "avx512", prefix, suffix);
+    else if (has_avx2()) add_library(os, "avx2", prefix, suffix);
+    else if (has_avx()) add_library(os, "avx", prefix, suffix);
+    add_library(os, "noavx", prefix, suffix);
+#elif defined(__APPLE__)
+#if TARGET_OS_VISION
+    add_library("visionos", "", "lib", "a");
+#elif TARGET_OS_IOS
+    add_library("ios", "", "lib", "a");
+#else
+    add_library("macos", "acc", "lib", "dylib");
+    add_library("macos", "no_acc", "lib", "dylib");
+#endif
+#elif defined(__ANDROID__)
+    add_library("android", "", "lib", "so");
+#endif
+    return architectures;
+}
+
+inline LibHandle load_library(const char* path) {
+    return LOAD_LIB(path);
+}
+
+inline void* load_symbol(LibHandle handle, const char* symbol) {
+    return GET_SYM(handle, symbol);
+}
+
+inline void unload_library(LibHandle handle) {
+    CLOSE_LIB(handle);
+}
+
+LibHandle load_library_safe(const std::string& path) {
+    if (setjmp(sigjmp_buf_point) != 0) {
+        std::cerr << "Error loading library: " << path << std::endl;
+        return nullptr;
+    }
+
+    LibHandle handle_out = load_library(path.c_str());
+    if (!handle_out) {
+        std::cerr << "Failed to load library: " << path << std::endl;
+    }
+    return handle_out;
+}
+
+bool LLMRuntime::create_LLM_library_from_path(const std::string& command, const std::string& path) {
+    std::cout << "Trying " << path << std::endl;
+
+    if (setjmp(sigjmp_buf_point) != 0) {
+        std::cout << "Error occurred while loading the library" << std::endl;
+        return false;
+    }
+
+    handle = load_library_safe(path);
+    if (!handle) return false;
+
+    auto load_sym = [&](auto& fn_ptr, const char* name) {
+        fn_ptr = reinterpret_cast<std::decay_t<decltype(fn_ptr)>>(load_symbol(handle, name));
+        if (!fn_ptr) {
+            std::cerr << "Failed to load: " << name << std::endl;
+        }
+    };
+
+#define DECLARE_AND_LOAD(name, ret, ...) \
+    load_sym(this->name, #name); \
+    if (!this->name) return false;
+    LLM_FUNCTIONS_LIST(DECLARE_AND_LOAD)
+#undef DECLARE_AND_LOAD
+
+    llm = (LLMProvider*) LLM_Construct(command.c_str());
+    return true;
+}
+
+bool LLMRuntime::create_LLM_library(const std::string& command, const std::string& path) {
+    bool gpu = false;
+
+    std::istringstream iss(command);
+    std::string arg;
+    while(iss >> arg) {
+        if (arg == "-ngl" || arg == "--gpu-layers" || arg == "--n-gpu-layers") {
+            gpu = true;
+            break;
+        }
+    }
+
+    std::vector<std::string> llmlibPaths;
+    if (is_file(path)) {
+        llmlibPaths.push_back(path);
+    } else {
+        for (const auto& arch : available_architectures(gpu)) llmlibPaths.push_back(join_paths(path, arch));
+    }
+
+    for (const auto& llmlibPath : llmlibPaths) {
+        if (setjmp(sigjmp_buf_point) != 0) {
+            std::cout << "Error occurred while loading llmlib: " << llmlibPath << ", trying next." << std::endl;
+            continue;
+        }
+
+        bool success = create_LLM_library_from_path(command, llmlibPath);
+        if (success) {
+            std::cout << "Successfully loaded: " << llmlibPath << std::endl;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+//============================= LLMRuntime =============================//
+
+LLMRuntime::LLMRuntime(const std::string& command, const std::string& path)
+{
+    set_error_handlers(true, false);
+    create_LLM_library(command, path);
+}
+
+LLMRuntime::LLMRuntime(const char* command, const std::string& path) : LLMRuntime(std::string(command), path) { }
+
+LLMRuntime::LLMRuntime(int argc, char ** argv, const std::string& path) : LLMRuntime(args_to_command(argc, argv), path) { }
+
+LLMRuntime::~LLMRuntime() {
+    if (llm) {
+        LLM_Delete(llm);
+        llm = nullptr;
+    }
+    if (handle) {
+        unload_library(handle);
+        handle = nullptr;
+    }
+}
+
+//============================= API =============================//
+
+const char* Available_Architectures(bool gpu)
+{
+    const std::vector<std::string>& llmlibs = available_architectures(gpu);
+    thread_local static std::string result;
+
+    std::ostringstream oss;
+    for (size_t i = 0; i < llmlibs.size(); ++i) {
+        if (i != 0) oss << ",";
+        oss << llmlibs[i];
+    }
+    result = oss.str();
+    return result.c_str();
+}
+
+LLMRuntime* LLMRuntime_Construct(const std::string& command, const std::string& path) {
+    LLMRuntime* lib = new LLMRuntime(command, path);
+    if(lib->llm == nullptr)
+    {
+        delete lib;
+        return nullptr;
+    }
+    return lib;
+}
