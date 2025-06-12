e839970994c31b91bb0314fb24e21f35a317068e
Antonis Makropoulos <benuix@gmail.com>
2025-06-12 15:26:10 +0300

    dynamic only implementation of server

--- llamalib_server.cpp
+++ llamalib_server.cpp
@@ -1,22 +1,15 @@
-#ifdef LLAMALIB_BUILD_RUNTIME_LIB
 #include "LLM_runtime.h"
-#else
-#include "LLM_service.h"
-#endif
 
 int main(int argc, char ** argv) {
     std::string command = args_to_command(argc, argv);
     std::cout << command << std::endl;
 
-#ifdef LLAMALIB_BUILD_RUNTIME_LIB
     LLMRuntime* llm = LLMRuntime_Construct(command);
     if (!llm) {
         std::cout << "Failed to load any backend." << std::endl;
         return 1;
     }
-#else
-    LLMService* llm = LLM_Construct(command.c_str());
-#endif
+
     LLM_Start(llm);
     LLM_Start_Server(llm);
     LLM_Join_Server(llm);
