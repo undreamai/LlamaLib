175fed7d049141cab28680b7a07603819cc5310d
Antonis Makropoulos <benuix@gmail.com>
2025-08-01 22:41:39 +0300

    doxygen setup

---
+++ .github/doxygen/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+download_path=/tmp/doxygen.tar.gz
+if [ ! -f $download_path ]; then
+  wget -O $download_path https://www.doxygen.nl/files/doxygen-1.10.0.linux.bin.tar.gz
+fi
+
+doxygen=/tmp/doxygen-1.10.0/bin/doxygen
+if [ ! -f $doxygen ]; then
+  tar -xvf $download_path -C /tmp
+fi
+
+if [ ! -f $doxygen ]; then
+  exit 1;
+fi
+
+$doxygen .github/doxygen/Doxyfile
+
+output_dir=html
+mkdir -p $output_dir/images/
+cp .github/*png .github/*.gif $output_dir/images/
+sed -i 's:LLamaLib:Package:g' $output_dir/navtreedata.js
